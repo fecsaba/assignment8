1c0b1dbf0ec7e64b9dab4da5df236138a18928d3
fecsaba <nexusegyesulet@gmail.com>
2020-01-10 10:42:30 +0100

    assignment8

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br>
+        <button class="btn btn-primary"
+          @click="selectedComponent='appBlue'">Load Blue Template</button>
+        <button class="btn btn-success"
+                @click="selectedComponent='appGreen'">Load Green Template</button>
+        <button class="btn btn-danger"
+                @click="selectedComponent='appRed'">Load Red Template</button>
+        <hr>
+        <component :is="selectedComponent">
+          <p>Content</p>
+        </component>
+<!--        <app-blue></app-blue>-->
+<!--        <app-green></app-green>-->
+<!--        <app-red></app-red>-->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import Blue from "@/components/Blue";
+import Green from "@/components/Green";
+import Red from "@/components/Red";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  },
+  data: function () {
+    return {selectedComponent: 'appRed'}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
